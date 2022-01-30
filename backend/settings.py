ac0434cdb6167ba9ac7aa096023ff4ffe63b4f4a
Maniek1892 <Elino1892@users.noreply.github.com>
2022-01-30 03:03:45 +0100

    Improve responsiveness all sites

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'https://extrans-sedziszow-volleyball.herokuapp.com/']
 
